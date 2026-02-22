850fb8fe05b9dffe1ff00c17238bde9302fbd68d
Maksim <ungamax321@gmail.com>
2026-02-22 18:38:51 +0700

    Update models.ts
    ПРИВЕТ

--- src/models.ts
+++ src/models.ts
@@ -1,19 +1,41 @@
-export interface IEntity {
+export interface IEntity{
     id: string;
     name: string;
 }
 
-export interface IIngredient extends IEntity {
+export interface IIngredient extends IEntity{
     price: number;
 }
 
-export interface IPizzaBase extends IEntity {
+export interface IPizzaBase extends IEntity{
     price: number;
     isClassic: boolean; 
 }
 
-export interface IPizza extends IEntity {
+export interface IPizza extends IEntity{
     base: IPizzaBase;
     ingredients: IIngredient[];
     totalPrice: number;
+    border?: IPizzaBortik;
+    size?: PizzaSize;
 }
+
+export interface IPizzaBortik extends IEntity{
+    ingredients: IIngredient[];
+    price: number;
+    allowedPizzaIds: string[]
+}
+
+export type PizzaSize = 'small' | 'medium' | 'large';
+
+
+export interface IOrder extends IEntity{
+    id: string;
+    items: IPizza[];
+    totalPrice: number;
+    comment: string;
+    createdAt: Date;
+    timeWhenNeeded: Date;
+}
+
+
