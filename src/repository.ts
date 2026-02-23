9c264a4632b584c7a24498dc0901c5801e4191c0
Maksim <ungamax321@gmail.com>
2026-02-24 00:03:54 +0700

    Update repository.ts

--- src/repository.ts
+++ src/repository.ts
@@ -1,18 +1,18 @@
 import {IEntity} from './models';
 
-export class Repository<T extends IEntity>{
-    private items: T[]=[]; 
-    create(item: T): void {
+export class Repository<ZOV extends IEntity>{
+    private items: ZOV[]=[]; 
+    create(item: ZOV): void {
         this.items.push(item);
     }
     
-    getAll():T[]{return this.items;}
+    getAll():ZOV[]{return this.items;}
 
-    getById(id: string): T|undefined{
+    getById(id: string): ZOV|undefined{
         return this.items.find(item=>item.id===id);}
 
     delete(id: string): void{this.items=this.items.filter(item=>item.id!==id);}
-    update(id: string, updates: Partial<T>): void {
+    update(id: string, updates: Partial<ZOV>): void {
         const index = this.items.findIndex(item => item.id === id);
         if (index!==-1){
             this.items[index]={...this.items[index],...updates};
