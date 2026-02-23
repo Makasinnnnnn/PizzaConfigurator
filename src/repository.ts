25fdde0acb8f542a1a1e5049e0433f61516ef565
Maksim <ungamax321@gmail.com>
2026-02-23 23:46:12 +0700

    Update repository.ts

--- src/repository.ts
+++ src/repository.ts
@@ -1,4 +1,4 @@
-import { IEntity } from './models';
+import {IEntity} from './models';
 
 export class Repository<T extends IEntity>{
     private items: T[]=[]; 
@@ -11,12 +11,11 @@ export class Repository<T extends IEntity>{
     getById(id: string): T|undefined{
         return this.items.find(item=>item.id===id);}
 
-    //оставляем все кроме элемента выбранного
     delete(id: string): void{this.items=this.items.filter(item=>item.id!==id);}
     update(id: string, updates: Partial<T>): void {
         const index = this.items.findIndex(item => item.id === id);
-        if (index !== -1){
-            this.items[index] = { ...this.items[index], ...updates};//апдейтим старый элем новыми данными
+        if (index!==-1){
+            this.items[index]={...this.items[index],...updates};
         }
     }
 }
